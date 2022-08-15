10d0c967e004fafe4b8881321899ff58c007fa3c
Tiago Neves <tiagoneves.tia@gmail.com>
2022-08-15 20:59:43 -0300

    feat: add command-line flags

---
+++ firstProgram/wc/README.md
@@ -0,0 +1,10 @@
+# WC
+
+```bash
+$ go build
+
+$ echo "My first command line tool with go" | ./wc
+
+$ cat main.go | ./wc
+
+```
--- firstProgram/wc/main.go
+++ firstProgram/wc/main.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	// Calling the count function to count the number of words
+	/**
+		 * By using command-line flags, you can extend the functionality
+	   * of your tools without compromising any flexibility for tour users.
+	*/
+
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	// Parsing the flags provided by the user
+	flag.Parse()
+	// Calling the count function to count the number of words (or lines)
 	// received from the Standard Input and printing it out.
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
-	// Define the scanner splint type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// If the count lines flag is not set, we want to count words so we define
+	// the scanner split type to words (default is split by lines)
+	if !countLines {
+		// Define the scanner splint type to words (default is split by lines)
+		scanner.Split(bufio.ScanWords)
+	}
+
 	// Defining a counter
 	wc := 0
 	// For every word scanned, increment the counter
